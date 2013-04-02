5529b681e51f30a08982f05ce0328a8946cbfbb7
MarcoMorawec <marco.morawec@gmail.com>
2013-04-02 18:12:43 -0400

    adjusted size of images in table to tumbnails

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -8,6 +8,6 @@ class Pin < ActiveRecord::Base
   								size: { less_than: 5.megabytes }
 
   belongs_to :user
-  has_attached_file :image
+  has_attached_file :image, styles: { medium: "320x240>", :thumb => "150x150>" }
 
 end
