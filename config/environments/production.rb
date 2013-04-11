1d057a781fabcae252f8a04b48fc42dceff3cb20
MarcoMorawec <marco.morawec@gmail.com>
2013-04-11 09:01:04 -0400

    adding S3 into production again - troubleshoot heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,4 +69,12 @@ Omrails::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
 
-
+# configuring amazon S3 for paperclip for file uploads
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
