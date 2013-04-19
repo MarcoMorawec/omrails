9dc7dffb15d7f565dbf0f1a5cdb429ee3b9c0c90
MarcoMorawec <marco.morawec@gmail.com>
2013-04-18 20:01:45 -0400

    adding user sign up form to the index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-	@pins = @user.pins.page(params[:page]).per_page(20)
+	   @pins = @user.pins.page(params[:page]).per_page(20)
+
+	  
+	  respond_to do |format|
+      	format.html # show.html.erb
+      	format.json { render json: @pins }
+      	format.js
+	     end
   end
 end
